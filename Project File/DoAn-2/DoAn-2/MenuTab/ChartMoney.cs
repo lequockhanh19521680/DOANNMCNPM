b377d45391a85e14bc914f33c81e6d184931e708
lequockhanh19521680 <86584033+lequockhanh19521680@users.noreply.github.com>
2021-07-01 17:36:59 +0700

    Update ChartMoney.cs

--- Project File/DoAn-2/DoAn-2/MenuTab/ChartMoney.cs
+++ Project File/DoAn-2/DoAn-2/MenuTab/ChartMoney.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,26 +20,26 @@ namespace DoAn_2.MenuTab
         public ChartMoney()
         {
             InitializeComponent();
-            ChartMoneydaybydate();
+            ChartMoneyDayByDate();
         }
 
-        private void ChartMoneydaybydate()
+        private void ChartMoneyDayByDate()
         {
-            chart1.Series["Series1"].XValueType = ChartValueType.DateTime;
+            ChartRead.Series["Series1"].XValueType = ChartValueType.DateTime;
 
-            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "dd-MM";
-            DataSet ds = new DataSet();
+            ChartRead.ChartAreas[0].AxisX.LabelStyle.Format = "dd-MM";
+            DataSet DanhSach = new DataSet();
             connect.Open();
             //SqlDataAdapter adapt = new SqlDataAdapter("SELECT CAST(HDtime AS DATE) AS Ngay,COUNT(value) as slsp, HDthanhtoan as tien FROM HoaDon CROSS APPLY STRING_SPLIT(HDmasp, ',') where MONTH(HDtime) = '"+ CurrentMonth + "' group by HDtime, HDthanhtoan ORDER BY HDtime", connect);
             SqlDataAdapter adapt = new SqlDataAdapter("select CAST(HDtime AS DATE) as Ngay, sum(HDthanhtoan) AS tien from HoaDon where MONTH(HDtime) = '" + currentMonth + "' group by CAST(HDtime AS DATE) ORDER by CAST(HDtime AS DATE)", connect);
 
-            adapt.Fill(ds);
-            chart1.DataSource = ds;
+            adapt.Fill(DanhSach);
+            ChartRead.DataSource = DanhSach;
             //set the member of the chart data source used to data bind to the X-values of the series  
-            chart1.Series["Series1"].XValueMember = "Ngay";
+            ChartRead.Series["Series1"].XValueMember = "Ngay";
             //set the member columns of the chart data source used to data bind to the X-values of the series  
-            chart1.Series["Series1"].YValueMembers = "tien";
-            chart1.Series["Series1"].IsValueShownAsLabel = true;
+            ChartRead.Series["Series1"].YValueMembers = "tien";
+            ChartRead.Series["Series1"].IsValueShownAsLabel = true;
             //ChartSlspDayByDay.Titles.Add("Salary Chart");
             connect.Close();
         }
